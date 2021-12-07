2ee3da9b8d32d15f2d367c597590df89d78e9795
Mirsan1996 <mirsaningenieria@gmail.com>
2021-12-07 10:24:06 -0600

    modificado:     graph_live/realtime_graph/requirements.txt 	modificado:     graph_live/realtime_graph/static/main.js 	modificado:     graph_live/realtime_graph/templates/base.html

--- graph_live/realtime_graph/static/main.js
+++ graph_live/realtime_graph/static/main.js
@@ -2,23 +2,58 @@ var ctx = document.getElementById('myChart').getContext('2d');
 
 var graphData = {
     type: 'line',
+
     options: {
         scale:{
             suggestedMin: 0,
             suggestedMax: 130
         }
     },
+
     data: {
-        labels: ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0',],
-        datasets: [{
-            label: 'GRAFICA TOTALMENTE EN VIVO 3',
+        labels: ['90', '90', '90', '90', '90', '90', '90', '90', '90', '90', '90', '90', '90', '90', '90', '90', '90', '90', '90', '90'],
+        datasets: [
+	{
+            label: 'PRESION',
             data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             backgroundColor: [
                 'rgba(30, 169, 244, 0.2)',
             ],
-            borderWidth: 5
-        }]
-    },
+            borderColor: [
+                'rgba(30, 169, 244, 0.2)',
+            ],
+            borderWidth: 5,
+            pointHitRadius: 20,
+	    pointBorderWidth: 10,
+        },
+	{
+            label: 'VOLTAJE',
+            data: [10, 12, 20, 29, 33, 37, 38, 41, 42, 54, 59, 67, 68, 71, 75, 77, 81, 86, 89, 99],
+            backgroundColor: [
+                'rgba(236, 247, 17, 0.2)',
+            ],
+            borderColor: [
+                'rgba(236, 247, 17, 0.2)',
+            ],
+            borderWidth: 5,
+            pointHitRadius: 20,
+	    pointBorderWidth: 10,
+	},
+	{
+            label: 'CORRIENTE',
+            data: [40, 12, 35, 57, 38, 2, 44, 56, 40, 40, 40, 40, 40, 40, 20, 35, 56, 45, 12, 36],
+            backgroundColor: [
+                'rgba(247, 17, 17, 0.2)',
+            ],
+            borderColor: [
+                'rgba(247, 17, 17, 0.2)',
+            ],
+            borderWidth: 5,
+            pointHitRadius: 20,
+	    pointBorderWidth: 10,
+	},
+		  ]
+    }
 }
 
 var myChart = new Chart(ctx, graphData);
